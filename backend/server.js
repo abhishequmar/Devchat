cc77c0f525119ce05e1e5c5e94c1a0d2a3110b9c
Hunain <272002abhishek@gmail.com>
2024-01-11 23:27:54 +0530

    changed folder structure

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,16 @@ const express = require('express');
 
 const notes = require( "./data/data.js");
 const dotenv = require( "dotenv");
+const userRoutes = require('./routes/userRoutes');
+const { useRoutes } = require('react-router-dom');
 
 const app = express();
 dotenv.config();
 app.get('/', (req, res) => {
     res.send("API is running");
-})
+});
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
 
-})
-
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n)=>n._id===req.params.id);
-    // res.json(notes);
-    console.log(req.params);
-    res.send(note);
-
-})
+app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log('server started on 5000'));
